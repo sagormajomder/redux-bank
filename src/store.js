613fd40ec9e8c1c8b98333ca2141ad3f8be62cf4
Sagor Majomder <sagormajomder@gmail.com>
2026-02-24 02:01:06 +0600

    implement action creators for legacy redux

--- src/store.js
+++ src/store.js
@@ -65,6 +65,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+/*
 store.dispatch({ type: 'account/deposit', payload: 500 });
 console.log(store.getState()); // current state of the store
 
@@ -78,4 +79,60 @@ store.dispatch({
 console.log(store.getState()); // current state of the store
 
 store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState()); // current state of the store
+*/
+
+//! Create Action  Creator
+
+//* Account Action Creator
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+function requestLoan(loanAmount, loanPurpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount: loanAmount, purpose: loanPurpose },
+  };
+}
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+console.log(store.getState()); // current state of the store
+
+store.dispatch(deposit(500));
+console.log(store.getState()); // current state of the store
+
+store.dispatch(withdraw(200));
+console.log(store.getState()); // current state of the store
+
+store.dispatch(requestLoan(1000, 'Buy a car'));
+console.log(store.getState()); // current state of the store
+
+store.dispatch(payLoan());
+console.log(store.getState()); // current state of the store
+
+//* Customer Action Creator
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+
+function updateName(fullname) {
+  return { type: 'customer/updateName', payload: fullname };
+}
+
+store.dispatch(createCustomer('Sagor Majomder', '452364'));
+console.log(store.getState()); // current state of the store
+
+store.dispatch(updateName('Sagor Mazumder'));
 console.log(store.getState()); // current state of the store
