5b066f644843c4924e1ea6692e9f49424de8a193
Sagor Majomder <sagormajomder@gmail.com>
2026-02-25 00:09:18 +0600

    Refactor store setup to use Redux Toolkit and remove deprecated dependencies

--- src/store-v2.js
+++ src/store-v2.js
@@ -1,14 +1,18 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
 
-import { configureStore } from '@reduxjs/toolkit';
-
-const store = configureStore({
-  reducer: {
-    account: accountReducer,
-    customer: customerReducer,
-  },
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
 
